90ebfa7f0cd6b2d6ae20e7c2392953a7e34fde3b
Aqil F <aqilfaizal2004@gmail.com>
2025-09-17 09:24:12 -0700

    Started on intro card

---
+++ website/src/components/introCard.js
@@ -0,0 +1,7 @@
+// Displays greeting
+
+function IntroCard() {
+  return (
+    <h2>Hi, my name is</h2>
+  );
+}
---
+++ website/src/containers/intro.js
@@ -0,0 +1,9 @@
+// Takes data from portfolio
+// Takes component for intro
+// Passes data as props to intro
+
+// Render IntroCard
+
+export default function Intro() {
+    return 0;
+}
