0cf70337ffd0f349e6fd8cfc86114767d0924e59
Aqil F <aqilfaizal2004@gmail.com>
2025-10-17 23:32:18 -0700

    Working out cloudlfare

--- website/src/containers/Main.js
+++ website/src/containers/Main.js
@@ -4,6 +4,10 @@ export default function Main() {
     return (
         <div className="main">
         <Intro />
+        <video width="100%" controls preload="metadata" poster="[URL_TO_POSTER_IMAGE]">
+            <source src="https://[YOUR_R2_PUBLIC_ENDPOINT]/my-video.mp4" type="video/mp4">
+            Your browser does not support the video tag.
+        </video>
         </div>
     );
 }
