a4fc8ee97b63a85ca23af6102c470bbce42105ca
Aqil F <aqilfaizal2004@gmail.com>
2025-10-21 18:37:24 -0700

    I CAN SEE

--- website/src/containers/Main.js
+++ website/src/containers/Main.js
@@ -4,8 +4,8 @@ export default function Main() {
     return (
         <div className="main">
         <Intro />
-        <video width="100%" controls preload="metadata" poster="[URL_TO_POSTER_IMAGE]">
-            <source src="https://pub-7a8aa7944aa34843b95f986417b76809.r2.dev/portrait_pics.mov" type="video/mp4">
+        <video width="20%"  autoPlay loop muted preload="metadata" poster="[URL_TO_POSTER_IMAGE]">
+            <source src="https://pub-7a8aa7944aa34843b95f986417b76809.r2.dev/portrait_pics.mov" type="video/mp4"/>
             Your browser does not support the video tag.
         </video>
         </div>
