acd804b2402937cd6245f5a189eabf862de8a063
Aqil F <aqilfaizal2004@gmail.com>
2025-10-23 14:33:04 -0700

    switched to public video on repo

--- website/src/containers/Main.js
+++ website/src/containers/Main.js
@@ -4,6 +4,7 @@ import "./Main.css";
 export default function Main() {
     return (
         <div className="main">
+        <Intro/>
         <Video/>
         </div>
     );
--- website/src/containers/Video/Video.js
+++ website/src/containers/Video/Video.js
@@ -6,7 +6,7 @@ const VideoContainer = () => {
   return (
     <div className="flex-container">
         <video width="100%"  autoPlay loop muted preload="metadata" poster="[URL_TO_POSTER_IMAGE]">
-            <source src="https://pub-7a8aa7944aa34843b95f986417b76809.r2.dev/horizontal_video.mov" type="video/mp4"/>
+            <source src="horizontal_video.mov" type="video/mp4"/>
             Your browser does not support the video tag.
         </video>
     </div>
